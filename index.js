50aab1e5286e17373c5afeb8949938f4eb69850f
nachods <nachodesimone36@gmail.com>
2024-09-10 12:38:45 -0300

    Mejoras de conexion

--- index.js
+++ index.js
@@ -7,26 +7,30 @@ const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
 const ipServer = process.env.IP_SERVER;
 const apiVersion = process.env.API_VERSION;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const connectDB = async () => {
   //Aca conecto la base de datos
   try {
-    //Aca indico donde se va a conectar, la direccion con los datos
-    await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}`);
-    app.listen(port, '0.0.0.0', () => {
+    // Conexión a MongoDB con parámetros adicionales
+    await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/miBaseDeDatos?retryWrites=true&w=majority`, // Cambia "miBaseDeDatos" por el nombre real de tu base
+      {
+        useNewUrlParser: true, // Estas opciones ayudan a prevenir problemas de conexión
+        useUnifiedTopology: true,
+      }
+    );
+    app.listen(port, () => {
       //Inicia el server con 'npm run dev'
       console.log("================================");
       console.log("============API RESET===========");
       console.log("================================");
       console.log(`http://${ipServer}:${port}/api/${apiVersion}`);
     });
-    console.log("La conexion con base de datos fue exitosa");
+    console.log("La conexión con base de datos fue exitosa");
   } catch (error) {
     console.log("Error al conectar la base de datos", error);
   }
 };
 
 connectDB();
-
-//npm i nodemon nos ayuda a actualizar el app de forma automatica, y no teniendo que reiniciar el server
